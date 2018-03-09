3f6c831cc7508ca1779e21857bdaf5335c14fe4f
tejalpadharia <36251006+tejalpadharia@users.noreply.github.com>
2018-03-09 15:16:26 -0800

    initial setup
    initial setup files and apis

---
+++ routes/calc.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+
+router.post('/doAdd', function (req, res, next) {
+    var Number1 = req.body.no1;
+    var Number2 = req.body.no2;
+
+    // Just checking if the username is in our user's array
+    var result = parseFloat(Number1) + parseFloat(Number2);
+
+        res.json({message: result});
+
+});
+
+
+router.post('/doSub', function (req, res, next) {
+    var Number1 = req.body.no1;
+    var Number2 = req.body.no2;
+
+    // Just checking if the username is in our user's array
+    var result = parseFloat(Number1) - parseFloat(Number2);
+
+    res.json({message: result});
+
+});
+
+
+router.post('/doMultiply', function (req, res, next) {
+    var Number1 = req.body.no1;
+    var Number2 = req.body.no2;
+
+    // Just checking if the username is in our user's array
+    var result = parseFloat(Number1) * parseFloat(Number2);
+    res.json({message: result});
+});
+
+
+router.post('/doDivide', function (req, res, next) {
+    var Number1 = req.body.no1;
+    var Number2 = req.body.no2;
+
+    // Just checking if the username is in our user's array
+    var result = parseFloat(Number1) / parseFloat(Number2);
+    res.json({message: result});
+});
+
+
+module.exports = router;
